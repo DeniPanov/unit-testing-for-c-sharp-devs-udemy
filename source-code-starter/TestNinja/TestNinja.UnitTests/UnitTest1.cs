8eb9a6acc5e9683bafd065a76da21dc2c18a7f7e
DeniPanov <Inedvonap@gmail.com>
2021-10-13 07:31:12 +0300

    My first tests; Add new line;

--- source-code-starter/TestNinja/TestNinja.UnitTests/UnitTest1.cs
+++ source-code-starter/TestNinja/TestNinja.UnitTests/UnitTest1.cs
@@ -19,5 +19,26 @@ namespace TestNinja.UnitTests
 
             Assert.IsTrue(result);
         }
+
+        [Test]
+        public void CanBeCanceledBy_SameUser_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user};
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCanceledBy_AnotherUser_ReturnsFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy( new User());
+
+            Assert.IsFalse(result);
+        }
     }
 }
