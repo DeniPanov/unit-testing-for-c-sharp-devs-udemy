43b24d24a978ed111a9fb703f21cf8ec25d33453
DeniPanov <Inedvonap@gmail.com>
2021-10-16 10:50:24 +0300

    Test strings, arrays and return types;

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.controller = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var result = this.controller.GetCustomer(0);
+
+            // Not found
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var result = this.controller.GetCustomer(1);
+
+            // Ok or instance of its derivatives
+            Assert.That(result, Is.InstanceOf<Ok>());
+        }
+    }
+}
